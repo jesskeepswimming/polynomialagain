eb18ffb69a010119195a767f03dbcf2188a31f7c
kateharvey <60273509+kateharvey@users.noreply.github.com>
2020-02-09 17:33:32 -0500

    working version

---
+++ polynomial.cpp
@@ -0,0 +1,319 @@
+#include <iostream>
+#include <vector>
+#include <string>
+//#include "lab1_polynomial.hpp"
+#include <fstream>
+#include <cassert>
+#define ASSERT_TRUE(T) if (!(T)) return false;
+#define ASSERT_FALSE(T) if((T)) return false;
+
+using namespace std;
+
+class Polynomial {
+    vector<int> data;
+
+public:
+    vector<int> get_data() {
+        return data;
+    }
+    
+    Polynomial(int A[], int size) {
+        data.resize(size);
+        for(int i = 0 ; i < size ; i ++) {
+            data[i] = A[i];
+        }
+    }
+
+    Polynomial() {
+        data.resize(rand() % 1000 + 1); // random degree in range [1, 1000]
+        for(int i = 0 ; i < data.size() ; i ++) {
+            data[i] = rand() % 2001 + (-1000); // make coefficients in range [-1000, 1000]
+        }
+    }
+
+    Polynomial(string file_name) {
+        ifstream my_file; // create file reader
+        my_file.open(file_name); // open the file
+        int coefficients;
+        int degree;
+        if(my_file.is_open()) {
+            my_file >> degree;
+
+            while(!my_file.eof()) {
+                my_file >> coefficients; // store each line of the file in coefficient variable
+                data.push_back(coefficients); // add coefficients to data
+            }
+            // number of terms in data cannot exceed the degree specified in the first line of the file
+            if(data.size() > degree) {
+                data.resize(degree);
+            }
+            my_file.close();
+        }
+    }
+
+    bool operator==(const Polynomial& target) {
+        bool are_equal = true;
+        if(data.size() != target.data.size()) return false;
+        for(int i = 0 ; i < data.size() ; i ++) {
+            if(data[i] != target.data[i]) return false;
+        }
+        return are_equal;
+    }
+
+    Polynomial operator+(const Polynomial& target) {
+        Polynomial highest_degree;
+        if(data.size() >= target.data.size()) highest_degree = *this;
+        else highest_degree = target;
+        int coefficients[highest_degree.data.size()];
+        
+        for(int i = 0 ; i < highest_degree.data.size() ; i ++) {
+            if(i >= data.size() || i >= target.data.size()) coefficients[i] = highest_degree.data[i];
+            else coefficients[i] = data[i] + target.data[i];
+        }
+        
+        Polynomial sum(coefficients, highest_degree.data.size());
+        return sum;
+    }
+
+    Polynomial operator-(const Polynomial& target) {
+        Polynomial highest_degree;
+        if(data.size() >= target.data.size()) highest_degree = *this;
+        else highest_degree = target;
+        int coefficients[highest_degree.data.size()];
+
+        for(int i = 0 ; i < highest_degree.data.size() ; i ++) {
+            if(i >= data.size()) coefficients[i] = -(highest_degree.data[i]);
+            else if(i >= target.data.size()) coefficients[i] = highest_degree.data[i];
+            else coefficients[i] = data[i] - target.data[i];
+        }
+        
+        Polynomial difference(coefficients, highest_degree.data.size());
+        return difference;
+    }
+
+    Polynomial operator*(const Polynomial& target) {
+
+        int size = data.size() + target.data.size() - 1;
+        int co[size];
+
+        for (int i = 0 ; i < size ; i ++) {
+            co[i] = 0;
+        }
+
+        for (int i = 0 ; i < target.data.size() ; i ++) {
+            for (int j = 0 ; j < data.size() ; j ++) co[i + j] += (data[j]*target.data[i]);
+        }
+        Polynomial product(co, size);
+        return product;
+    }
+
+    Polynomial derivative() {
+        int deriv_coefficients[data.size() - 1];
+        for(int i = 0 ; i < data.size() - 1 ; i ++) {
+            deriv_coefficients[i] = data[i + 1]*(i + 1);
+        }
+        
+        Polynomial derivative(deriv_coefficients, data.size() - 1);
+        return derivative;
+    }
+
+    void print() {
+        for(int i = data.size() - 1 ; i >= 0 ; i --) {
+            // first term
+            if(i == data.size() - 1) {
+                if(data[0] == 0) {};
+                cout << data[i] << "x^" << i;
+            }
+
+            // last term (constant)
+            else if(i == 0) {
+                if(data[i] > 0) cout << " + " << data[i];
+                else if(data[i] < 0) cout << " - " << abs(data[i]);
+            }
+
+            // middle terms
+            else {
+                if(data[i] > 0) cout << " + " << data[i] << "x^" << i;
+                else if(data[i] < 0) cout << " - " << abs(data[i]) << "x^" << i;
+            }
+        }
+    }
+    // ~Polynomial();
+    friend class PolynomialTest;
+};
+
+class PolynomialTest {
+    int poly_1[3] = {3, -2, 5};
+    int poly_2[4] = {-1, -2, 4, 4};
+    Polynomial test_polynomial_1;
+    Polynomial test_polynomial_2;
+    
+public:
+    void setup() {
+        cout << endl;
+        // 5x^2 - 2x + 3
+        test_polynomial_1.data.resize(0);
+        test_polynomial_1.data.push_back(poly_1[0]);
+        test_polynomial_1.data.push_back(poly_1[1]);
+        test_polynomial_1.data.push_back(poly_1[2]);
+
+        // 4x^3 + 4x^2 - 2x - 1
+        test_polynomial_2.data.resize(0);
+        test_polynomial_2.data.push_back(poly_2[0]);
+        test_polynomial_2.data.push_back(poly_2[1]);
+        test_polynomial_2.data.push_back(poly_2[2]);
+        test_polynomial_2.data.push_back(poly_2[3]);
+
+        assert(test_polynomial_1.data.size() == 3);
+        cout << "Size test passed for TP1 \n";
+        
+        assert(test_polynomial_2.data.size() == 4);
+        cout << "Size test passed for TP2 \n";
+        cout << endl;
+    }
+
+    void test_string_constructor() {
+        Polynomial file_1("test_polynomial.txt");
+        file_1.print();
+        cout << endl;
+        cout << "Polynomial degree: " << file_1.data.size() - 1;
+        cout << endl;
+
+        assert(file_1.data.size() == 6);
+        cout << "Test 1 for string constructor passed: polynomial degree is correct \n";
+
+        assert(file_1.data[0] == 6);
+        assert(file_1.data[1] == 5);
+        assert(file_1.data[2] == 4);
+        assert(file_1.data[3] == 3);
+        assert(file_1.data[4] == 2);
+        assert(file_1.data[5] == 1);
+        cout << "Test 2 for string constructor passed: coefficients read from file and stored correctly \n";
+
+        Polynomial file_2("test_polynomial_2.txt");
+        assert(file_2.data.size() == 3);
+        cout << "Test 3 for string constructor passed: data is correctly resized when there are too many terms in the file \n";
+
+        assert(file_2.data[0] == 4);
+        assert(file_2.data[1] == 5);
+        assert(file_2.data[2] == 6);
+        cout << "Test 4 for string constructor passed: coefficients read from file and stored correctly (extra terms are disregarded) \n";
+        cout << endl;
+    }
+
+    void test_default_constructor() {
+        Polynomial random;
+        Polynomial random_2;
+
+        assert(random.data.size() > 0 && random.data.size() < 1001);
+        cout << "Test 1 for default constructor passed: constructor correctly creates a polynomial of degree 1000 \n";
+        
+        for(int i = 0 ; i < random.data.size() ; i ++) {
+            assert(random.data[i] <= 1000);
+            assert(random.data[i] >= -1000);
+        }
+        cout << "Test 2 for default constructor passed: coefficients are in range [-1000, 1000] \n";
+
+        assert(!(random == random_2));
+        cout << "Test 3 for default constructor passed: constructor creates a different polynomial each time \n";
+        cout << endl;
+    }
+
+    // test empty polynomial
+    // test addition/subtraction/mult/equality of 0 polynomials
+
+    void test_equal() {
+        Polynomial p1(poly_1, 3);
+
+        assert(p1.operator==(test_polynomial_1) == true);
+        cout << "Test 1 for operator== passed: polynomials are equal \n";
+
+        p1.data[0] = -3;
+        assert(p1.operator==(test_polynomial_1) == false);
+        cout << "Test 2 for operator== passed: polynomials with different signs not equal \n";
+
+        p1.data.pop_back();
+        assert(p1.operator==(test_polynomial_1) == false);
+        cout << "Test 3 for operator== passed: polynomials not equal \n";
+    }
+
+    void test_addition() {
+        assert(test_polynomial_1.operator+(test_polynomial_2).data.size() == 4);
+        cout << "Test 1 for operator+ passed: degree of sum polynomial is correct \n";
+
+        assert(test_polynomial_1.operator+(test_polynomial_2).data[0] == 2);
+        cout << "Test 2 for operator+ passed: constant term of sum polynomial is correct \n";
+
+        assert(test_polynomial_1.operator+(test_polynomial_2).data[3] == 4);
+        cout << "Test 3 for operator+ passed: x^2 term of sum polynomial has correct coefficient \n";
+        cout << endl;
+    }
+
+    void test_subtraction() {
+        assert(test_polynomial_1.operator-(test_polynomial_2).data.size() == 4);
+        cout << "Test 1 for operator- passed: degree of poylnomial is correct \n";
+
+        assert(test_polynomial_1.operator-(test_polynomial_2).data[0] == 4);
+        assert(test_polynomial_1.operator-(test_polynomial_2).data[1] == 0);
+        assert(test_polynomial_1.operator-(test_polynomial_2).data[2] == 1);
+        assert(test_polynomial_1.operator-(test_polynomial_2).data[3] == -4);
+        cout << "Test 2 for operator- passed: all coefficients are correct for TP1 - TP2 \n";
+
+        assert(test_polynomial_2.operator-(test_polynomial_1).data[0] == -4);
+        assert(test_polynomial_2.operator-(test_polynomial_1).data[1] == 0);
+        assert(test_polynomial_2.operator-(test_polynomial_1).data[2] == -1);
+        assert(test_polynomial_2.operator-(test_polynomial_1).data[3] == 4);
+        cout << "Test 3 for operator- passed: all coefficients are correct for TP2 - TP1 \n";
+
+        Polynomial zero(0, 0);
+        assert(test_polynomial_1.operator-(zero) == test_polynomial_1);
+        cout << "Test 4 for operator- passed: subtracting zero polynomial does not change the original \n";
+        cout << endl;
+    }
+
+    void test_multiplication() { // jess
+
+
+    }
+
+    void test_derivative() { // jess
+
+    }
+
+    void run() {
+        setup();
+        test_string_constructor();
+        test_default_constructor();
+
+        test_equal();
+        test_addition();
+        test_subtraction();
+    }
+};
+
+int main() {
+    int a_coefficients[4] = {1, 2, 3, 4};
+    Polynomial a(a_coefficients, 4);
+    int b_coefficients[3] = {4, 5, 7};
+    Polynomial b(b_coefficients, 3);
+
+    a.print();
+    cout << endl;
+    b.print();
+    cout << endl;
+    
+    /*
+    a.operator+(b).print();
+    cout << endl;
+    
+    a.operator-(b).print();
+    cout << endl;
+    a.derivative().print();
+    cout << endl;
+    a.operator*(b).print();
+    cout << endl;
+    */
+
+    PolynomialTest test_1;
+    test_1.run();
+}
